eca343622c3d2e64e60891f78658eb2a3661485b
Kris <xericue@yahoo.com>
2026-03-29 08:40:48 -0400

    loop

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -10,24 +10,31 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is {}... Shhh ...", secret_number);
-    println!("Enter your guess:");
+    
+    loop {
+        println!("Enter your guess:");
 
-    let mut guess = String::new();
-    // hi!
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
+        // hi!
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("dawg type a number...");
+        let guess: u32 = guess.trim().parse().expect("dawg type a number...");
 
-    println!("You guessed: {guess}");
+    
+        println!("You guessed: {guess}");
 
-    // Ordering is another enum (type with multiple variants) and thus
-    // we define its outcome with each
+        // Ordering is another enum (type with multiple variants) and thus
+        // we define its outcome with each
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small :("),
-        Ordering::Greater => println!("TOO BIG!"),
-        Ordering::Equal => println!("PEAK!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small :("),
+            Ordering::Greater => println!("TOO BIG!"),
+            Ordering::Equal => {
+                println!("PEAK!");
+                break;
+            }
+        }
     }
 }
